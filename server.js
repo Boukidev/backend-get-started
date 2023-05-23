f57e70f0dc1b32bf5cb4ee17973a29c51162af3f
Boukidev <boukidev@gmail.com>
2023-05-23 12:28:52 +0200

    Format code

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ const errorHandler = (error) => {
   if (error.syscall !== "listen") throw error;
 
   const address = server.address();
-  const bind =
-    typeof address === "string" ? "pipe " + address : "port: " + port;
+  const bind = typeof address === "string" ? "pipe " + address : "port: " + port;
   switch (error.code) {
     case "EACCES":
       console.error(bind + " requires elevated privileges.");
