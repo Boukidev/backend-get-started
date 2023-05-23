a31348470cdad0eaa7a64ae596ffeab09a36f0ff
Boukidev <boukidev@gmail.com>
2023-05-23 20:13:21 +0200

    Delete files

--- src/routes/sendmail.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const sendmailCtrl = require("../controllers/sendmail.js");
-
-const router = express.Router();
-
-router.post("/api/sendmail", sendmailCtrl.sendMail);
-
-module.exports = router;
