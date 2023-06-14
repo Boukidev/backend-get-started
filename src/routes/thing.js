8501efec6e2cf55ab609411f71c35520cb646dc7
boukidev <boukidev@gmail.com>
2023-06-14 20:37:02 +0200

    Fix: Add auth verify for GET(/:id), POST, PUT & DELETE routes

--- src/routes/thing.js
+++ src/routes/thing.js
@@ -5,9 +5,9 @@ const auth = require("../middleware/auth.js");
 const router = express.Router();
 
 router.get("/", auth.jwtAuthMiddleware, thingCtrl.getThings);
-router.post("/", thingCtrl.postThing);
-router.get("/:id", thingCtrl.getThing);
-router.put("/:id", thingCtrl.putThing);
-router.delete("/:id", thingCtrl.deleteThing);
+router.post("/", auth.jwtAuthMiddleware, thingCtrl.postThing);
+router.get("/:id", auth.jwtAuthMiddleware, thingCtrl.getThing);
+router.put("/:id", auth.jwtAuthMiddleware, thingCtrl.putThing);
+router.delete("/:id", auth.jwtAuthMiddleware, thingCtrl.deleteThing);
 
 module.exports = router;
